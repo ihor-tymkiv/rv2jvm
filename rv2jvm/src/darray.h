98a5d38f2a9eab4b663430a4d14c249fb5eb68e1
Ihor Tymkiv <102431570+ihor-tymkiv@users.noreply.github.com>
2025-10-11 17:15:29 +0200

    Fix bugs in darray.h

--- rv2jvm/src/darray.h
+++ rv2jvm/src/darray.h
@@ -1,18 +1,27 @@
 #ifndef RV2JVM_DARRAY_H
 #define RV2JVM_DARRAY_H
 
-#define darray_append(darr, item) \
-	do { \
-		if (darr.capacity == 0) { \
-			darr.capacity = 64; \
-		} \
-		if (darr.capacity <= darr.size) { \
-			darr.capacity *= 2; \
-			darr.items = realloc(darr.items, \
-					     darr.capacity * sizeof(*darr.items)); \
-		} \
-		darr[darr.size] = item; \
-		darr.size++; \
+#include <stdlib.h>
+#include <stdio.h>
+
+#define darray_grow(darr)						   \
+	do {								   \
+		if (darr.capacity == 0) {				   \
+			darr.capacity = 64;				   \
+		} else {						   \
+			darr.capacity *= 2;				   \
+		}							   \
+		darr.items = realloc(darr.items,			   \
+				     darr.capacity * sizeof(*darr.items)); \
+	} while (0)
+
+#define darray_append(darr, item)			\
+	do {						\
+		if (darr.capacity <= darr.size) {	\
+			darray_grow(darr);		\
+		}					\
+		darr.items[darr.size] = item;		\
+		darr.size++;				\
 	} while (0)
 
 #endif
